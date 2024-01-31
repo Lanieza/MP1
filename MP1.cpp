f522a40c5f32014ca39086d623436c0b616c5d0b
Lanieza <laniezalopez08@gmail.com>
2024-01-31 11:59:18 +0800

    parsing strings

--- MP1.cpp
+++ MP1.cpp
@@ -9,68 +9,68 @@
 using namespace std; 
 
 
-int getType(string str){
-    if(str == "int" || str == "char" || str == "double" || str == "float"){
-        return 0; // for datatype;
-    }
-    else if(str == " "){
-        return 1; //for space;
-    }
-    else if(isIdentifier(str))
-        return 2;
-    else if(str == ","){
-        return 3; //for space;
-    }
-    else if(str == ";"){
-        return 4; //for space;
-    }else 
-        return 5;
+// int getType(string str){
+//     if(str == "int" || str == "char" || str == "double" || str == "float"){
+//         return 0; // for datatype;
+//     }
+//     else if(str == " "){
+//         return 1; //for space;
+//     }
+//     else if(isIdentifier(str))
+//         return 2;
+//     else if(str == ","){
+//         return 3; //for space;
+//     }
+//     else if(str == ";"){
+//         return 4; //for space;
+//     }else 
+//         return 5;
     
         
-}
+// }
 
-bool isIdentifier(string str){
-    //should start with underscore or any of the letter uppercase or lowercase letters
-    if(str[0] == '_' || str[0] >= 'A' && str[0] <= 'Z' && || str[0] >= 'a' && str[0] <= 'z' && ){
-        for(int i=1 ; i<str.length(); i++){
-            if (!(str[i] == '_' )|| (str[i] >= 'A' && str[i] <= 'Z') || (str[i] >= 'a' && str[i] <= 'z' ) || (str[i] >= '0' && str[i] <= '9' )){
-                return false;
-            }
-        }
+// bool isIdentifier(string str){
+//     //should start with underscore or any of the letter uppercase or lowercase letters
+//     if(str[0] == '_' || str[0] >= 'A' && str[0] <= 'Z' && || str[0] >= 'a' && str[0] <= 'z' && ){
+//         for(int i=1 ; i<str.length(); i++){
+//             if (!(str[i] == '_' )|| (str[i] >= 'A' && str[i] <= 'Z') || (str[i] >= 'a' && str[i] <= 'z' ) || (str[i] >= '0' && str[i] <= '9' )){
+//                 return false;
+//             }
+//         }
 
-        return true; //meaning all characters were checked
-    }else
-        return false;
+//         return true; //meaning all characters were checked
+//     }else
+//         return false;
 
-}
+// }
 
 
-bool check_Variable(const vector<string>& tokens) { // bool jud ni siya dapat
-    // I CANNOT MAKE THE STATE AS INDEX SINCE FOR SAMPLE
-    //INT        X; THERE WOULD BE MANY BLANK SPACES PA BEFORE X 
-    //SO UNYA RA MAG UPDATE OG STATE IF NANA KOS LETTERS EXCEPT SA MGA DELIMITERS
+// bool check_Variable(const vector<string>& tokens) { // bool jud ni siya dapat
+//     // I CANNOT MAKE THE STATE AS INDEX SINCE FOR SAMPLE
+//     //INT        X; THERE WOULD BE MANY BLANK SPACES PA BEFORE X 
+//     //SO UNYA RA MAG UPDATE OG STATE IF NANA KOS LETTERS EXCEPT SA MGA DELIMITERS
     
-    int state = 0, i=0;
+//     int state = 0, i=0;
     
-    for (auto it = tokens.begin(); it != tokens.end(); ++it){
-        int inputType = getType(*it);
+//     for (auto it = tokens.begin(); it != tokens.end(); ++it){
+//         int inputType = getType(*it);
 
-        if(state == 0 )
-    }
+//         if(state == 0 )
+//     }
 
-    cout<<endl;
+//     cout<<endl;
 
 
-    // if(state == 1) //meaning only the datype is the output
-    //     return false;
-    //another case is if naay comma dayon walay nisunod na variable
-    //
+//     // if(state == 1) //meaning only the datype is the output
+//     //     return false;
+//     //another case is if naay comma dayon walay nisunod na variable
+//     //
     
-    return true;
-    //if theres space mo continue 
-    //if theres comma mo continue 
-    //if theres ; then will stop
-}
+//     return true;
+//     //if theres space mo continue 
+//     //if theres comma mo continue 
+//     //if theres ; then will stop
+// }
 
 
 
@@ -102,15 +102,15 @@ void tokenize(const string& input, vector<string>& tokens, const string& delimit
 }
 
 int main() {
-    int cases;
-    cin >> cases;
+    // int cases;
+    // cin >> cases;
 
-    while(cases--){
+    // while(cases--){
         string inputString;
-        int testType;
-        cin >> testType;  // Read the type of the test case (1 or 2)
+        // int testType;
+        // cin >> testType;  // Read the type of the test case (1 or 2)
 
-        cin.ignore();  // i cite nig apil sa honor code 
+        // cin.ignore();  // i cite nig apil sa honor code 
 
         getline(cin, inputString);
         vector<string> tokens;
@@ -122,22 +122,22 @@ int main() {
         tokenize(inputString, tokens, delimiters);
 
         // Display the tokens and delimiters
-        // cout << "Tokens: ";
-        // for (const auto& token : tokens)
-        //     cout << "[" << token << "] ";
+        cout << "Tokens: ";
+        for (const auto& token : tokens)
+            cout << "[" << token << "] ";
 
-        // cout<<endl;
+        cout<<endl;
 
-        if (testType == 1) {
-            // Handle variable declaration
-            // check_Variable(tokens);
-            if (check_Variable(tokens)) {
-                cout << "VALID VARIABLE DECLARATION" << endl;
-            } else {
-                cout << "INVALID VARIABLE DECLARATION" << endl;
-            }
+        // if (testType == 1) {
+        //     // Handle variable declaration
+        //     // check_Variable(tokens);
+        //     if (check_Variable(tokens)) {
+        //         cout << "VALID VARIABLE DECLARATION" << endl;
+        //     } else {
+        //         cout << "INVALID VARIABLE DECLARATION" << endl;
+        //     }
 
-        } 
+        // } 
         //else if (testType == 2) {
         //     // Handle function declaration
         //     if (check_Function(tokens)) {
@@ -147,7 +147,7 @@ int main() {
         //     }
         // }
 
-    }
+    // }
 
     return 0;
 }
