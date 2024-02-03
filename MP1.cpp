0bec465157a96e505f49b23dabc4c79c16faff82
Lanieza <laniezalopez08@gmail.com>
2024-02-04 01:30:33 +0800

    implemented is valid literal and state
    does not have is valid fn yet and is valid declaration variable some cases not covered

--- MP1.cpp
+++ MP1.cpp
@@ -1,8 +1,12 @@
+//so take note that the int can be a number or single character char if and only if maconvert siya sa ascii
+//so the char can be a number from 0 to 255 and then a single character char na ma qoute
+//ang double or float kay any number basta usa ra kabuok ang dot
 
 #include <iostream>
 #include <sstream>
 #include <vector>
 #include <cctype>
+#include <algorithm> // Include the algorithm header for all_of used isidentifier function
 using namespace std;
 
 
@@ -24,37 +28,84 @@ enum TokenType {
     UNEXPECTED, //7
 };
 
+// enum data_type{
+//     INT, CHAR, FLOAT, DOUBLE,
+// };
+
 class Token {
 public:
     string identifier; // for example int
     TokenType token_type = START; // Specify the type for TokenType
     State token_state = STAGE_1; //
+    // string datatype = " ";
+
     
 };
 
-// bool isIdentifier(const std::string &str) {
-//     if (!str.empty() && (str[0] == '_' || isalpha(str[0]))) {
-//         for (char c : str.substr(1)) {
-//             if (!isalnum(c) && c != '_') {
-//                 return false;
-//             }
-//         }
-//         return true; // All characters were checked
-//     }
-//     return false;
-// }
+//----------------------------------------------------------------
 
-bool isIdentifier(const std::string &str) {
-    if (!str.empty() && (str[0] == '_' || isalnum(str[0]))) {
-        for (char c : str.substr(1)) {
-            if (!isalnum(c) && c != '_') {
-                return false;
-            }
+bool isvalid_int(const std::string& str) {
+    if (str.size() == 3 && str[0] == '\'' && str[2] == '\'') {
+        int asciiValue = static_cast<int>(str[1]);
+        return (asciiValue >= '0' && asciiValue <= '9');
+    } else {
+        // Try to convert the string to an integer
+        try {
+            size_t pos;
+            int intValue = std::stoi(str, &pos);
+            return pos == str.size(); // Check if the entire string was converted
+        } catch (...) {
+            return false; // Failed to convert to an integer
         }
-        return true; // All characters were checked
+    }
+}
+
+bool isvalid_float_double(const std::string &str) {
+    try {
+        std::size_t pos;
+        std::stod(str, &pos);
+        return pos == str.length();
+    } catch (const std::invalid_argument &) {
+        return false;
+    } catch (const std::out_of_range &) {
+        return false;
+    }
+}
+
+bool isvalid_char(const std::string &str) {
+    if (str.size() == 3 && str[0] == '\'' && str[2] == '\'') {
+        char asciiChar = str[1];
+        if (isdigit(asciiChar)) {
+            return true;
+        }
     }
     return false;
 }
+
+//----------------------------------------------------------------
+
+
+bool isIdentifier(const std::string &str) {
+    if (!str.empty()) {
+        // Check if the string is only a number
+        if (all_of(str.begin(), str.end(), ::isdigit)) {
+            return false;
+        }
+
+        // Check if the first character is an underscore or an alphabet
+        if (str[0] == '_' || std::isalpha(str[0])) {
+            // Check the rest of the characters
+            for (char c : str.substr(1)) {
+                if (!std::isalnum(c) && c != '_') {
+                    return false;
+                }
+            }
+            return true; // All characters were checked
+        }
+    }
+    return false;
+}
+
 
 
 int getType(string str) {
@@ -90,13 +141,8 @@ void tokenize(const string& input, vector<string>& tokens, const string& delimit
         string delimiter = input.substr(end, 1);
         tokens.push_back(delimiter);
 
-        // Move to the next token (skip delimiters)
+        // Move to the next token
         start = end + 1;
-        while (start < input.length() && delimiters.find(input[start]) != string::npos) {
-            start++;
-        }
-
-        // Find the next delimiter
         end = input.find_first_of(delimiters, start);
     }
 
@@ -106,7 +152,6 @@ void tokenize(const string& input, vector<string>& tokens, const string& delimit
         tokens.push_back(lastToken);
     }
 }
-
 
 bool check_Variable(const vector<string>& parsed_string, vector<Token>& tokens) {
     
@@ -159,6 +204,7 @@ bool check_Variable(const vector<string>& parsed_string, vector<Token>& tokens) 
     }
     
     State currentState = STAGE_1;
+    string datatype;
 
     for (auto& token : tokens) { 
 
@@ -170,6 +216,7 @@ bool check_Variable(const vector<string>& parsed_string, vector<Token>& tokens) 
             }else if(currentState == STAGE_1 && token.token_type == DATATYPE){
                 //cout<<"hello"<<endl;
                 currentState = STAGE_1;
+                datatype = token.identifier; // i stored the datatype
             }else if(currentState == STAGE_1 && token.token_type == VARNAME){
                 //cout<<"hello"<<endl;
                 currentState = STAGE_2;
@@ -177,20 +224,50 @@ bool check_Variable(const vector<string>& parsed_string, vector<Token>& tokens) 
                 currentState = STAGE_1;
             }else if(currentState == STAGE_2 && token.token_type == SEMICOLON){
                 currentState = STAGE_3;
+                datatype = " ";
             }else if(currentState == STAGE_2 && token.token_type == EQUAL_SIGN){
                 //2 cases that are valid: 
                 //1. value assigned is a declared variable
                 //2. value assigned is related to the datatype
                 currentState = STAGE_0; //means we have to check first if the initialize valus is valid
         
-            }
-            else if(currentState == STAGE_0 && token.token_type == VARNAME || token.token_type == UNEXPECTED){
+            }else if(currentState == STAGE_0 && token.token_type == VARNAME || token.token_type == UNEXPECTED){
                 
+                if(token.token_type == VARNAME){
+
+                    for (auto& t : tokens) {
+                        //checking if its declared
+                        if(token.token_type  == t.token_type && token.identifier == t.identifier){
+                            currentState = STAGE_2;
+                        }else 
+                            break;
+                    }
+                
+                }else if(token.token_type == UNEXPECTED){
+                    
+
+                    if(datatype == "int"){
+                        if(isvalid_int(token.identifier)){
+                            currentState = STAGE_2; 
+                        }else 
+                            return false;
+                    }else if(datatype == "char"){
+                        if(isvalid_char(token.identifier)){
+                            currentState = STAGE_2; 
+                        }else 
+                            return false;
+                    }else if(datatype == "float" || datatype == "double")
+                        if(isvalid_float_double(token.identifier))
+                            currentState = STAGE_2; 
+                        else 
+                            return false;
+                }
 
             }
-    }
 
-    cout<<"state: "<<currentState<<endl;
+        }
+
+    cout<<"STATE: "<<currentState<<endl;
 
     // Check if the last state is STAGE_3
     return currentState == STAGE_3;
@@ -214,7 +291,7 @@ int main() {
         vector<string> parsed_string;
 
         // Define a set of delimiters
-        string delimiters = ",; =";
+        string delimiters = ",;= ";
 
         // Tokenize the string using the custom function
         tokenize(inputString, parsed_string, delimiters);
